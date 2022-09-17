321bcc9a037a349a6f2caa0e84cf43155f00b2b0
Rohit Bisht <rbtunes0@gmail.com>
2022-09-18 00:22:00 +0530

    ok

--- client.py
+++ client.py
@@ -4,7 +4,8 @@ import os
 import sys
 file=sys.argv[1]
 file2=sys.argv[2]
+url='https://5000-rohitbisht-asrbackend-mh87fvqbwpf.ws-us65.gitpod.io'
 with open(file, 'rb') as f:
-    r = requests.post('https://5000-rohitbisht-me-8sqwd0i2h0v.ws-eu63.gitpod.io', files = {'file': open(file, 'rb')})
+    r = requests.post(url, files = {'file': open(file, 'rb')})
     with open(file2,'w',encoding='utf-8')as t:
       t.write(r.text)
